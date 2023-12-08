f0ea1937316a514925894904d576c65bc52721e1
KevalPolara <kevalpolara5272@gmail.com>
2023-12-08 11:46:59 +0530

    firebase_image_upoload_file

--- src/redux/slice/appoinment.slice.js
+++ src/redux/slice/appoinment.slice.js
@@ -1,103 +1,172 @@
-import { async } from "@firebase/util"
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
-import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, updateDoc } from "firebase/firestore"
-import { db } from "../../firebase/firebase"
-import { postAppoinmentData } from "../../common/api/appoinment.api"
-
+import { async } from "@firebase/util";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  updateDoc
+} from "firebase/firestore";
+import { db, storage } from "../../firebase/firebase";
+import {
+  deleteObject,
+  getDownloadURL,
+  ref,
+  uploadBytes
+} from "firebase/storage";
 
 const initialState = {
-    isLaoding : false,
-    error : null,
-    appoinment : []
-}
+  isLaoding: false,
+  error: null,
+  appoinment: []
+};
 
+export const addApt = createAsyncThunk("appoinment/add", async data => {
+  console.log(data);
+  const number = Math.floor(Math.random() * 10000);
 
-export const  addAppoinmentData = createAsyncThunk(
+  let aptdata = { ...data };
 
-    'appoinment/post',   
- 
-    async (data) =>{ 
-    const docRef = await addDoc(collection(db, "appoinemnts"), data);
+  const storageRef = ref(storage, "apt/" + number + "_" + data.pres.name);
 
+  console.log(storageRef);
+  // 'file' comes from the Blob or File API
+  await uploadBytes(storageRef, data.pres)
+    .then(async snapshot => {
+      await getDownloadURL(ref(storage, snapshot.ref)).then(async url => {
+        console.log(url);
 
-      console.log("Document written with ID: ", docRef.id);
-    }
-    
-)
-
-export const getAppoinmentData = createAsyncThunk(
-    'appoinment/get',
-
-    async () =>{
-
-        await new Promise((resolve,reject) => setTimeout(resolve,1000))
-
-        let data = []
-
-        const querySnapshot = await getDocs(collection(db, "appoinemnts"),data);
-        querySnapshot.forEach((doc) => {
-            data.push({...doc.data(),id:doc.id})
-          // doc.data() is never undefined for query doc snapshots
-          console.log(doc.id, " => ", doc.data());
+        let aptdoc = await addDoc(collection(db, "apt"), {
+          ...data,
+          dataname: number + "_" + data.pres.name,
+          pres: url
         });
 
-        console.log(data);
+        console.log(aptdoc);
 
-        return data;
-        
-    }
-  
-)
+        aptdata = {
+          id: aptdoc.id,
+          ...data,
+          dataname: number + "_" + data.pres.name,
+          pres: url
+        };
+
+        console.log(aptdata);
+      });
+    })
+    .catch(error => console.log(error));
+
+  return aptdata;
+});
+
+export const addAppoinmentData = createAsyncThunk(
+  "appoinment/post",
+  async data => {
+    console.log(data);
+
+    const docRef = await addDoc(collection(db, "appoinemnts"), data);
+
+    console.log("Document written with ID: ", docRef.id);
+  }
+);
+
+export const getAppoinmentData = createAsyncThunk(
+  "appoinment/get",
+  async () => {
+    await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+
+    let data = [];
+
+    const querySnapshot = await getDocs(collection(db, "appoinemnts"), data);
+    querySnapshot.forEach(doc => {
+      data.push({ ...doc.data(), id: doc.id });
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    });
+
+    console.log(data);
+
+    return data;
+  }
+);
 
 export const deleteAppoinmentData = createAsyncThunk(
-    'appoinment/delete',
+  "appoinment/delete",
+  async id => {
+    try {
+      const filePath = 
+      // Create a reference to the file to delete
+      const desertRef = ref(storage, );
 
-    async (id) =>{
-        await deleteDoc(doc(db, "appoinemnts", id));
-        }
-)
+      // Delete the file
+    await deleteObject(desertRef)
+        .then(() => {
+
+          return id;
+          // File deleted successfully
+        })
+        .catch(error => {
+          // Uh-oh, an error occurred!
+        });
+    } catch (e) {}
+  }
+);
 
 export const editAppoinmentData = createAsyncThunk(
-    'appoinment/put',
-    
-    async (data)=>{
-    const washingtonRef = doc(db, "appoinemnts",data.id);
+  "appoinment/put",
+  async data => {
+    const washingtonRef = doc(db, "appoinemnts", data.id);
     await updateDoc(washingtonRef, data);
-    }
-)
+  }
+);
 
 export const appoinmentSlice = createSlice({
-    name : 'appoinment',
-    initialState,
-    reducers : {
+  name: "appoinment",
+  initialState,
+  reducers: {},
 
-    },
-    
-    extraReducers : (builder) =>  {
-        builder.addCase(addAppoinmentData.fulfilled,(state,action)=>{
-            state.isLaoding = false
-            state.error = null 
-            state.appoinment = action.payload
-        })
-        builder.addCase(getAppoinmentData.fulfilled,(state,action)=>{
-            state.isLaoding = false
-            state.error = null 
-            state.appoinment = action.payload
-        })
+  extraReducers: builder => {
+    // builder.addCase(addAppoinmentData.fulfilled, (state, action) => {
+    //   state.isLaoding = false;
+    //   state.error = null;
+    //   state.appoinment = action.payload;
+    // });
+    // builder.addCase(getAppoinmentData.fulfilled, (state, action) => {
+    //   state.isLaoding = false;
+    //   state.error = null;
+    //   state.appoinment = action.payload;
+    // });
+    // builder.addCase(deleteAppoinmentData.fulfilled, (state, action) => {
+    //   console.log(action);
+    //   state.isLaoding = false;
+    //   state.error = null;
+    //   state.appoinment = action.payload;
+    // });
+    // builder.addCase(editAppoinmentData.fulfilled, (state, action) => {
+    //   state.isLaoding = false;
+    //   state.error = null;
+    //   state.appoinment = action.payload;
+    // });
 
-        builder.addCase(deleteAppoinmentData.fulfilled,(state,action)=>{
-            console.log(action);
-            state.isLaoding = false
-            state.error = null 
-            state.appoinment = action.payload
-        })
+    // builder.addCase(addApt.fulfilled, (state, action) => {
+    //   console.log(state, action.payload);
+    //   state.isLaoding = false;
+    //   state.error = null;
+    //   state.appoinment = state.appoinment.concat(action.payload);
+    // });
 
-        builder.addCase(editAppoinmentData.fulfilled,(state,action)=>{
-            state.isLaoding = false
-            state.error = null 
-            state.appoinment = action.payload
-        })
-    }
-})
+    builder.addCase(addApt.fulfilled, (state, action) => {
+      state.isLaoding = false;
+      state.error = null;
+      if (Array.isArray(state.appoinment)) {
+        state.appoinment = state.appoinment.concat(action.payload);
+      } else {
+        state.appoinment = [action.payload]; // If not an array, create a new array with the payload
+      }
+    });
+  }
+});
 
 export default appoinmentSlice.reducer;
--- src/redux/slice/cart.slice.js
+++ src/redux/slice/cart.slice.js
@@ -1,59 +1,63 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 
-const initialState={
+const initialState = {
+  cart: []
+};
 
-    cart :[]
-}
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      console.log(action.payload);
+      let check = state.cart.some(v => v.id === action.payload.id);
+      console.log(check);
 
-export const cartSlice=createSlice({
-    name : 'cart',
-    initialState,
-    reducers:{
-        addToCart:(state,action)=>{
-            let check = state.cart.some(v => v.id === action.payload.id);
-            console.log(check);
-      
-            let index = state.cart.findIndex(v => v.id === action.payload.id);
-            console.log(index);
-      
-            if (check) {
-              state.cart[index].qty++;
-            } else {
-              state.cart.push(action.payload);
-            }
+      let index = state.cart.findIndex(v => v.id === action.payload.id);
+      console.log(index);
 
-            state.cart=state.cart
-        } ,
+      if (check) {
+        state.cart[index].qty++;
+        console.log(state.cart[index].qty);
+      } else {
+        state.cart.push(action.payload);
+      }
 
-        incrementCart : (state,action)=>{
-          let index1=state.cart.findIndex(v=>v.id===action.payload);
-          state.cart[index1].qty++; 
-          
-          state.cart=state.cart
-        }, 
-        
-        decrementCart: (state,action)=>{
-          let index2=state.cart.findIndex(v=>v.id===action.payload);
+      state.cart = state.cart;
+      console.log(state.cart);
 
-          if( state.cart[index2].qty > 1){
-          state.cart[index2].qty--;
-          }
+    },
 
-          state.cart=state.cart
+    incrementCart: (state, action) => {
+      let index1 = state.cart.findIndex(v => v.id === action.payload);
+      state.cart[index1].qty++;
 
-        },
+      state.cart = state.cart;
+    },
 
-        deleteCart: (state,action)=>{
+    decrementCart: (state, action) => {
+      let index2 = state.cart.findIndex(v => v.id === action.payload);
 
-          state.cart=state.cart.filter((v)=>v.id!==action.payload);          
+      if (state.cart[index2].qty > 1) {
+        state.cart[index2].qty--;
+      }
 
-          state.cart=state.cart
-        }
-        
-        
+      state.cart = state.cart;
+    },
 
+    deleteCart: (state, action) => {
+      state.cart = state.cart.filter(v => v.id !== action.payload);
+
+      state.cart = state.cart;
     }
-})
+  },
 
-export const {addToCart,incrementCart,decrementCart,deleteCart}=cartSlice.actions
-export default cartSlice.reducer
+});
+
+export const {
+  addToCart,
+  incrementCart,
+  decrementCart,
+  deleteCart
+} = cartSlice.actions;
+export default cartSlice.reducer;
